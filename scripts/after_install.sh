5b7f421f0c3946829b104dd6f92f40dd8d5aaed3
agreg0ire <artgreg@outlook.fr>
2015-12-16 15:31:41 +0100

    - changed syntax bash for test purposes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-sudo chmod -R 777 /Users/art/Documents/tampoon.net/pdf
-sudo chmod -R 777 /Users/art/Documents/tampoon.net/csv
-sudo chmod -R 777 /Users/art/Documents/tampoon.net/htmTemplates
+sudo chmod -R 777 /Users/art/Documents/tampoon.net/{csv,pdf,htmTemplates}
